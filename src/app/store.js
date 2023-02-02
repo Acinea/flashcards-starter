afe2743d5ecdae554ea252b50e789b0f52e39790
James Kennedy <acinea.kennedy@gmail.com>
2023-02-02 10:50:15 -0500

    Added LocalSave Ability

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,26 @@ import { configureStore } from "@reduxjs/toolkit";
 import { default as topicsReducers } from "../features/topics/topicsSlice"
 import {default as quizzesReducers} from "../features/quizzes/quizzesSlice"
 import {default as cardsReducers} from '../features/cards/cardsSlice'
+import { debounce } from "../components/debounce";
+import { loadState, saveState } from "../components/localStorage";
 
-export default configureStore({
+const persistedState = loadState();
+
+export const store = configureStore({
   reducer: {
     topics: topicsReducers,
     quizzes: quizzesReducers,
     cards: cardsReducers
   },
+  preloadedState: persistedState
 });
+
+store.subscribe(
+	debounce(() => {
+		saveState({
+			topics: store.getState().topics,
+			quizzes: store.getState().quizzes,
+			cards: store.getState().cards,
+		});
+	}, 1000)
+);
